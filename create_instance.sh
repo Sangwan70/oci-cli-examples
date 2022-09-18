d10c3475db24f379f3d62733d3cfa9c7b0d09d28
Ram Niwas Sangwan <ramnsangwan@gmail.com>
2022-09-18 21:44:42 +0530

    Update create_instance.sh

--- create_instance.sh
+++ create_instance.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
-# Author Brokedba https://twitter.com/BrokeDba
-echo "******* Oci instance launch ! ************"
+echo "******* OCI instance launch ! ************"
 echo "Choose your Shape ||{**}||" 
 echo
-oci compute shape list --cid $C --output table --query "sort_by(data[?contains("shape",'VM.Standard.E2.1.Micro')],&\"shape\") [*].{ShapeName:shape,Memory:\"memory-in-gbs\",CPUcores:ocpus}"
+oci compute shape list -c $C --output table --query "sort_by(data[?contains("shape",'VM.Standard2.1')],&\"shape\") [*].{ShapeName:shape,Memory:\"memory-in-gbs\",CPUcores:ocpus}"
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 BLUE='\033[1;34m'
 echo -e "Note: If ${RED} VM.Standard2.1${GREEN} is not listed that means that your FreeTier trial is over ${BLUE}[Default option =Micro compute]"
-read -p "Enter the Shape name you wish to create [VM.Standard.E2.1.Micro]: " shape
-shape=${shape:-"VM.Standard.E2.1.Micro"}
+read -p "Enter the Shape name you wish to create [VM.Standard2.1: " shape
+shape=${shape:-"VM.Standard2.1"}
 
-read -p "Enter the Path of your ssh key [/c/Users/brokedba/oci/.ssh/id_rsa.pub]: " public_key
-public_key=${public_key:-/c/oracle/oci/.ssh/id_rsa.pub}  # this is a GITbash path
+read -p "Enter the Path of your ssh key [~/.ssh/id_rsa.pub]: " public_key
+public_key=${public_key:-~/.ssh/id_rsa.pub}  # this is a default path
 read -p "Enter the name of your new Instance ["Demo-Cli-Instance"]: " instance_name
 instance_name=${instance_name:-"Demo-Cli-Instance"}
  echo selected Instance name : $sub_name
@@ -27,27 +26,27 @@ select opt in "${options[@]}"
 do
   case $opt in
         "Oracle-Linux")
-          oci compute image list --cid $C --query "reverse(sort_by(data[?contains(\"display-name\",'Oracle-Linux')],&\"time-created\")) |[0:1].{ImageName:\"display-name\", OCID:id, OS:\"operating-system\", Size:\"size-in-mbs\",time:\"time-created\"}" --output table
+          oci compute image list -c $C --query "reverse(sort_by(data[?contains(\"display-name\",'Oracle-Linux')],&\"time-created\")) |[0:1].{ImageName:\"display-name\", OCID:id, OS:\"operating-system\", Size:\"size-in-mbs\",time:\"time-created\"}" --output table
           ocid_img=$(oci compute image list -c $C --query "reverse(sort_by(data[?contains(\"display-name\",'Oracle-Linux')],&\"time-created\")) |[0].id" --raw-output) 
           break
           ;;
         "CentOS")
-          oci compute image list --cid $C --query "reverse(sort_by(data[?contains(\"display-name\",'CentOS')],&\"time-created\")) |[0:1].{ImageName:\"display-name\", OCID:id, OS:\"operating-system\", Size:\"size-in-mbs\",time:\"time-created\"}" --output table
+          oci compute image list -c $C --query "reverse(sort_by(data[?contains(\"display-name\",'CentOS')],&\"time-created\")) |[0:1].{ImageName:\"display-name\", OCID:id, OS:\"operating-system\", Size:\"size-in-mbs\",time:\"time-created\"}" --output table
           ocid_img=$(oci compute image list -c $C --query "reverse(sort_by(data[?contains(\"display-name\",'CentOS')],&\"time-created\")) |[0].id" --raw-output)
           break
           ;;
         "Oracle Autonomus Linux")
-          oci compute image list --cid $C --query "reverse(sort_by(data[?contains(\"display-name\",'Oracle-Autonomous-Linux')],&\"time-created\")) |[0:1].{ImageName:\"display-name\", OCID:id, OS:\"operating-system\", Size:\"size-in-mbs\",time:\"time-created\"}" --output table
+          oci compute image list -c $C --query "reverse(sort_by(data[?contains(\"display-name\",'Oracle-Autonomous-Linux')],&\"time-created\")) |[0:1].{ImageName:\"display-name\", OCID:id, OS:\"operating-system\", Size:\"size-in-mbs\",time:\"time-created\"}" --output table
           ocid_img=$(oci compute image list -c $C --query "reverse(sort_by(data[?contains(\"display-name\",'Oracle-Autonomous-Linux')],&\"time-created\")) |[0].id" --raw-output)
           break
           ;;
         "Ubuntu")
-          oci compute image list --cid $C --query "reverse(sort_by(data[?contains(\"display-name\",'Canonical-Ubuntu')],&\"time-created\")) |[0:1].{ImageName:\"display-name\", OCID:id, OS:\"operating-system\", Size:\"size-in-mbs\",time:\"time-created\"}" --output table
+          oci compute image list -c $C --query "reverse(sort_by(data[?contains(\"display-name\",'Canonical-Ubuntu')],&\"time-created\")) |[0:1].{ImageName:\"display-name\", OCID:id, OS:\"operating-system\", Size:\"size-in-mbs\",time:\"time-created\"}" --output table
           ocid_img=$(oci compute image list -c $C --query "reverse(sort_by(data[?contains(\"display-name\",'Canonical-Ubuntu')],&\"time-created\")) |[0].id" --raw-output)
           break
           ;;
         "Windows")
-          oci compute image list --cid $C --query "reverse(sort_by(data[?contains(\"display-name\",'Windows')],&\"time-created\")) |[0:1].{ImageName:\"display-name\", OCID:id, OS:\"operating-system\", Size:\"size-in-mbs\",time:\"time-created\"}" --output table
+          oci compute image list -c $C --query "reverse(sort_by(data[?contains(\"display-name\",'Windows')],&\"time-created\")) |[0:1].{ImageName:\"display-name\", OCID:id, OS:\"operating-system\", Size:\"size-in-mbs\",time:\"time-created\"}" --output table
           ocid_img=$(oci compute image list -c $C --query "reverse(sort_by(data[?contains(\"display-name\",'Windows')],&\"time-created\")) |[0].id" --raw-output)
           break
           ;;         
@@ -58,7 +57,7 @@ do
   esac
 done
 echo "********** Network ***********"
-# ocid_img=$(oci compute image list -c $C --operating-system "Oracle Linux" --operating-system-version "7.8" --shape $shape --query 'data[0].id' --raw-output) 
+# ocid_img=$(oci compute image list -c $C --operating-system "Oracle Linux" --operating-system-version "7.9" --shape $shape --query 'data[0].id' --raw-output) 
 while true; do
  oci network vcn list -c $C --output table --query "data[*].{CIDR:\"cidr-block\", VCN_NAME:\"display-name\", DOMAIN_NAME:\"vcn-domain-name\", DNS:\"dns-label\"}"
  read -p "select the VCN for your new instance [$vcn_name]: " vcn_name
